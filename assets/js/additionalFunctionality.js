dc22c63728cd64758dad69318b17b92cf373efe4
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2023-06-24 15:57:35 -0400

    Clean up before presenting update

--- assets/js/additionalFunctionality.js
+++ assets/js/additionalFunctionality.js
@@ -6,6 +6,7 @@ readMoreElements.forEach(function(element) {
     paragraph.classList.toggle('expanded');
     if (paragraph.classList.contains('expanded')) {
       paragraph.style.maxHeight = 'none';
+      paragraph.style.transition = 'max-height 2s ease'
       this.textContent = 'Read Less';
     } else {
       paragraph.style.maxHeight = '3.6em'; // Adjust this value to display the desired number of lines
