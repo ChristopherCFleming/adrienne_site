70c78e2e7ec1858a79a54bad1809729749f81847
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2023-07-07 02:27:29 -0400

    Adjust read more and read less with +, -, and create Spanish Version

--- assets/js/additionalFunctionality.js
+++ assets/js/additionalFunctionality.js
@@ -7,10 +7,10 @@ readMoreElements.forEach(function(element) {
     if (paragraph.classList.contains('expanded')) {
       paragraph.style.maxHeight = 'none';
       paragraph.style.transition = 'max-height 2s ease'
-      this.textContent = 'Read Less';
+      this.textContent = '-';
     } else {
       paragraph.style.maxHeight = '3.6em'; // Adjust this value to display the desired number of lines
-      this.textContent = 'Read More';
+      this.textContent = '+';
     }
   });
 });
