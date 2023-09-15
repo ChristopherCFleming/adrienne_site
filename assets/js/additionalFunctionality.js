93235cd220c62a0a42379c9128a48cb9c6373f42
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2023-09-14 23:20:15 -0400

    Adjust JavaScript to only add functionality after the DOM content is loaded

--- assets/js/additionalFunctionality.js
+++ assets/js/additionalFunctionality.js
@@ -1,25 +1,5 @@
-// The functions below are for the old hidden functionality on the main page
-
-var readMoreElements = document.querySelectorAll('.read-more');
-
-readMoreElements.forEach(function(element) {
-  element.addEventListener('click', function() {
-    var paragraph = this.previousElementSibling;
-    paragraph.classList.toggle('expanded');
-    if (paragraph.classList.contains('expanded')) {
-      paragraph.style.maxHeight = 'none';
-      paragraph.style.transition = 'max-height 2s ease'
-      this.textContent = 'Read Less';
-    } else {
-      paragraph.style.maxHeight = '4em'; // Adjust this value to display the desired number of lines
-      this.textContent = 'Read More';
-    }
-  });
-});
-
 
 // The functions below are for the new functionaity on the Policy page:
-
 
 document.addEventListener("DOMContentLoaded", function() {
     const accordions = document.querySelectorAll('.accordion');
@@ -42,5 +22,26 @@ document.addEventListener("DOMContentLoaded", function() {
         expand.addEventListener('click', toggleAccordion.bind(accordion));
     });
 });
+
+// The functions below are for the old hidden functionality on the main page
+
+// var readMoreElements = document.querySelectorAll('.read-more');
+
+// readMoreElements.forEach(function(element) {
+//   element.addEventListener('click', function() {
+//     var paragraph = this.previousElementSibling;
+//     paragraph.classList.toggle('expanded');
+//     if (paragraph.classList.contains('expanded')) {
+//       paragraph.style.maxHeight = 'none';
+//       paragraph.style.transition = 'max-height 2s ease'
+//       this.textContent = 'Read Less';
+//     } else {
+//       paragraph.style.maxHeight = '4em'; // Adjust this value to display the desired number of lines
+//       this.textContent = 'Read More';
+//     }
+//   });
+// });
+
+
 
 
